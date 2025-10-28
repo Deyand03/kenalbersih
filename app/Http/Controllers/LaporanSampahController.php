a5bff457c4e56d7c1bede5a609d02eff819c83e1
Deyand03 <defryyandy7@gmail.com>
2025-10-28 20:54:14 +0700

    menambahkan migration, model dan controller

---
+++ app/Http/Controllers/LaporanSampahController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LaporanSampah;
+use Illuminate\Http\Request;
+
+class LaporanSampahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LaporanSampah $laporanSampah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LaporanSampah $laporanSampah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LaporanSampah $laporanSampah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LaporanSampah $laporanSampah)
+    {
+        //
+    }
+}
