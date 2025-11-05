b8de31333b3b3ef65aa9dd4a721536d27645ad41
rsmgf <gerdofiero@email.com>
2025-11-05 22:15:02 +0700

    new file:   public/img/image 2.png 	modified:   resources/views/auth/login.blade.php 	modified:   resources/views/auth/register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,111 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link 
+        href="https://fonts.googleapis.com/css2?family=Irish+Grover&family=Parisienne&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik+Glitch&family=Saira+Stencil+One&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" 
+        rel="stylesheet">
+</head>
+
+<body style="font-family: 'Poppins', sans-serif;">
+    <div class="flex max-w-100vw w-full">
+        {{-- Left Section --}}
+        <div class="relative overflow-hidden h-screen">
+            <div class="h-screen bg-no-repeat bg-cover bg-center flex flex-col"
+            style="background-image: url('{{ asset('svg/Group 13.svg') }}')">
+            <div class="flex flex-col w-1/2 justify-between h-full px-16 py-8">
+            <h1 class="text-4xl font-bold mb-6 text-[#E1EEBC]">INILAH MY IUNO</h1>
+            <p class="text-2xl pr-12 text-white">
+                Iuno in Wuthering Waves is a 5 Stars Aero character who wields a Gauntlets as their weapon type.
+                She is favored by fate under one name, and swept away by it in another. Yet never once has she been defeated, nor has she ever yielded. Now, witness the defiant Priestess. Witness what keeps her striding forward, never to look back. Witness how she walks the intricate web of destiny and becomes the only answer.
+            </p>
+            <div class="flex items-center space-x-4">
+                <p class="text-[#E1EEBC] italic">Teamwork makes the dream work</p>
+            </div>
+            </div>
+
+            </div>
+        </div>
+
+        {{-- Right Section --}}
+        <div class="absolute top-0 right-0 w-1/2 h-full flex items-center justify-center p-8">
+
+            <p class="text-3xl absolute top-8 right-8 pr-5 text-[#016B61] font-bold">
+    
+                K<span style="font-family: 'Parisienne', cursive; font-weight: normal;">enal</span>&nbsp;B<span style="font-family: 'Parisienne', cursive; font-weight: normal;">ersih</span>
+
+            </p>
+
+            <div class="w-full max-w-md">
+
+                <h2 class="text-xl mb-4 text-center text-[#328E6E]">Selamat datang</h2>
+                <p class="text-sm text-gray-500 mb-8 text-center px-12">Mari melangkah untuk menjaga dan menciptakan Lingkungan RT yang asri bersama.</p>
+            
+              <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <!-- Name -->
+                    <div>
+                        <x-input-label for="name" :value="__('Name')" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full"
+                                        type="password"
+                                        name="password"
+                                        required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                        type="password"
+                                        name="password_confirmation" required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                            {{ __('Already registered?') }}
+                        </a>
+
+                        <x-primary-button class="ms-4">
+                            {{ __('Register') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +156,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
