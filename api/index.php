70aec1bc013b4b36dde8e1870a78e3b3b4f51b74
Deyand03 <defryyandy7@gmail.com>
2025-11-23 13:16:24 +0700

    config vercel

--- api/index.php
+++ api/index.php
@@ -1,11 +1,2 @@
 <?php
-
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
-// 1. Arahkan ke public/index.php yang asli
 require __DIR__ . '/../public/index.php';
