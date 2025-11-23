f0e966bd0c32b4ae19ff9a7229f1ab0547c54e6b
Deyand03 <defryyandy7@gmail.com>
2025-11-23 14:08:26 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,24 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/style.css',
+                'resources/css/admin.css',
+                'resources/js/app.js',
+                'resources/js/bootstrap.js',
+                'resources/js/homepage.js',
+                'resources/js/jadwal_admin.js',
+                'resources/js/utility/chart_admin.js',
+                'resources/js/utility/chart_homepage.js',
+                'resources/js/utility/fetch_tahun.js',
+                'resources/js/utility/iuran_rt.js',
+                'resources/js/utility/jadwal_angkut.js',
+                'resources/js/utility/laporan_pengeluaran.js',
+                'resources/js/utility/navbar_iuran.js',
+                'resources/js/utility/navbar_lapor_sampah.js',
+                'resources/js/utility/profile.js',
+            ],
             refresh: true,
         }),
     ],
